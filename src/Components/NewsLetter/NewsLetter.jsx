6fd3de4a74138c038984e4fa3e185ee8999fac68
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-14 05:16:09 +0100

    Add News letter to my shop component

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./NewsLetter.css"
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get exclusive offer on your email</h1>
+        <p>Subscribe to our news Letter and stay updated</p>
+        <div>
+            <input type="email" name="" id="" placeholder='enter your email Id' />
+            <button>Subscribe Now   </button>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
