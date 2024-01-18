fba475cba7ebc557d62920bc9457a2b35df84650
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-18 17:08:03 +0100

    add Formik package and also some function login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import "./CSS/LoginSignUp.css"
+import React, { useState } from "react";
+import "./CSS/LoginSignUp.css";
 
 const LoginSignup = () => {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+
+  const onChangeHandler =(event)=>{
+    setFormData(()=>({
+      ...formData,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  const handleContinue =(e)=>{
+    e.preventDefault();
+    console.log(formData)
+  }
   return (
-    <div className="loginsignup">
+    <form className="loginsignup">
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
-          <input type="text" name="" id="" placeholder="Your name" />
-          <input type="email" name="" id="" placeholder="Your Email" />
-          <input type="password" name="" id="" placeholder="Your Pasword" />
-        </div>
-        <button>Continue</button>
+          <input onChange={onChangeHandler} type="text" name="text" id="nameId" placeholder="Your name" />
+          <input onChange={onChangeHandler} type="email" name="email" id="emailId" placeholder="Your Email" />
+          <input onChange={onChangeHandler} type="password" name="password" id="passId" placeholder="Your Pasword" />
+        </div>  
+        <button onClick={handleContinue}>Continue</button>
         <p className="loginsignup-login">
           Already have an account <span>Login here!</span>
         </p>
@@ -20,7 +38,7 @@ const LoginSignup = () => {
           <p>By continuing, I agree to the terms and use & privacy policy</p>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
