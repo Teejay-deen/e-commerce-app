d520557149ed67ed6665192e70c25b34ae24e2e4
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-18 02:42:12 +0100

    add cart functionality

--- src/Components/CartItems/CartItem.jsx
+++ src/Components/CartItems/CartItem.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./CartItem.css";
+import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItem = () => {
+  const { all_product, CartItems, removeToCart } = useContext(ShopContext);
   return (
-    <div>CartItem</div>
-  )
-}
+    <div className="cartitem">
+      <div className="cartitems-format-main">
+        <p>Product</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (CartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitem-format">
+                <img className="carticon-product-icon" src={e.image} alt="" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitem-quantity">{CartItems[e.id]}</button>
+                <p>{e.new_price * CartItems[e.id]}</p>
+                <img
+                  onClick={() => {
+                    removeToCart(e.id);
+                  }}
+                  src={remove_icon}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
