e3652f59b9fe522019eb19f28e4ebfbc07dbdc4c
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-01-23 10:39:50 +0100

    some bug solved

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -27,6 +27,7 @@ const ShopCategory = (props) => {
             id={item.id}
             name={item.name}
             image={item.image}
+            
             new_price={item.new_price}
             old_price={item.old_price}/>
           }
