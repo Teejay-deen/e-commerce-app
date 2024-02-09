49f78a7f149c279daa3a752c87265db83a294fd2
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-02-09 12:16:47 +0100

    adding style to my hompage components

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import "./CSS/Homepage.css";
+import user_avatar from "../Components/Assets/user-avatar.jpg";
 
 const HomePage = () => {
   return (
-    <div className='home-'></div>
+    <div className='home-screen'>
+        {/* <header>
+            <h3>System name</h3>
+            <a href="#">Logout</a>
+        </header> */}
+
+
+        <div className="page-content">
+            <div className="user-card">
+                <img src={user_avatar} alt="user-avatar" />
+                <h3>User Full Name</h3>
+                <div className="user-meta">
+                    <p>
+                        <span>Username</span>
+                        <span>Test Username</span>
+                    </p>
+
+                    <p>
+                        <span>Email</span>
+                        <span>test@gmail.com</span>
+                    </p>
+
+                    <p>
+                        <span>Gender</span>
+                        <span>Male or Female</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
